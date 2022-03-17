b233576c86fbeb5b6ab793b082a4b5f648e9fedd
mathias.ramilo <mathias.ramilo@fing.edu.uy>
2022-03-17 19:13:19 -0300

    Main Modal Finished

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+// INITIAL DATA MODAL
+
+// 1. Get the necessary elements from the DOM
+const initialDataSubmit = document.getElementById('initial-data-submit');
+
+initialDataSubmit.addEventListener('click', () => {
+    // Get all the input values
+    let name = document.getElementById('name').value;
+    let lastname = document.getElementById('lastname').value;
+    let age = document.getElementById('age').value;
+    let gender = document.getElementById('gender').value;
+    let height = document.getElementById('height').value;
+    let weight = document.getElementById('weight').value;
+
+    // Form Validation
+    if ()
+})
